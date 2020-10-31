c2163b7fd8d1c95cb38fc0ef6fc67752838cd2ba
hong yuseok <jericoh@naver.com>
2020-10-31 22:04:57 +0900

    graphql lecture 6

--- movieQL/graphql/resolver.js
+++ movieQL/graphql/resolver.js
@@ -1,8 +1,18 @@
+const hys = {
+    name : "HYS",
+    age : 17,
+    gender : "male"
+};
+
+
 // query를 해결하는 부분
 const resolver={
     Query:{
-        // 누군가 name이라는 query를 보내면 hys라고 응답함
-        name:()=>"hys"
+        /*
+            누군가 name이라는 query를 보내면 hys라고 응답함
+            name:()=>"hys",     
+        */
+       person: () => hys
     }
 }
 
